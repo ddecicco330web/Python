e46e6a0ba35d170a7720ea9914a3dba2c7043021
ddecicco330web <ddecicco330@gmail.com>
2023-12-07 18:42:54 -0500

    Exercise 1.3

---
+++ Achievement1/Exercise1.3/Exercise_1.3.py
@@ -0,0 +1,47 @@
+recipes_list = []
+ingredients_list = []
+
+def take_recipe():
+    name = input("Enter the name of the recipe: ")
+    cooking_time = int(input("Enter the cooking time: "))
+    ingredients = list(input("Enter the ingredients separated by commas: ").split(", "))
+    recipe = {"name": name, "cooking_time": cooking_time, "ingredients": ingredients}
+    print("")
+    return recipe
+
+n = int(input("How many recipes do you want to enter? "))
+
+for i in range(n):
+    recipe = take_recipe()
+
+    for ingredient in recipe["ingredients"]:
+        if ingredient not in ingredients_list:
+            ingredients_list.append(ingredient)
+
+    recipes_list.append(recipe)
+
+for recipe in recipes_list:
+    difficulty = "Not Set"
+    if recipe["cooking_time"] < 10 and len(recipe["ingredients"]) < 4:
+        difficulty = "Easy"
+    elif recipe["cooking_time"] < 10 and len(recipe["ingredients"]) >= 4:
+        difficulty = "Medium"
+    elif recipe["cooking_time"] >= 10 and len(recipe["ingredients"]) < 4:
+        difficulty = "Intermediate"
+    elif recipe["cooking_time"] >= 10 and len(recipe["ingredients"]) >= 4:
+        difficulty = "Hard"
+
+    print("")
+    print("Recipe: " + recipe["name"])
+    print("Cooking Time(min): " + str(recipe["cooking_time"]))
+    
+    print("Ingredients: ")
+    for ingredient in recipe["ingredients"]:
+        print(ingredient)
+    print("Difficulty: " + difficulty)
+    print("")
+
+print("Ingredients Accross All Recipes: ")
+print("--------------------")
+for ingredient in ingredients_list:
+    print(ingredient)
